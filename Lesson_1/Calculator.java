6192f7eb7c1edae0c22f3051bb4d20277c672bbb
KororteevStanislav <koroteevsd@gmail.com>
2023-01-27 06:13:51 +0300

    Добавил на проверку ДЗ Калькулятор

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 4;
+        char sign = '*';
+        int result = 0;
+
+        if(num1 > 0 && num2 > 0) {
+            if(sign == '+') {
+                result = num1 + num2;
+            } else if(sign == '-') {
+                result = num1 - num2;
+            } else if(sign == '*') {
+                result = num1 * num2;
+            } else if(sign == '/') {
+                result = num1 / num2;
+            } else if(sign == '%') {
+                result = num1 % num2;
+            } else if(sign == '^') {
+                result = 1;
+                for(int i = num2; i != 0; i--) {
+                    result = result * num1;
+                }
+            }
+        }
+        
+        System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+    }
+}
