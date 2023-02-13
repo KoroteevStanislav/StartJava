f29911314aa1b23a068b04b7ce4375e8a3ab60d5
KororteevStanislav <koroteevsd@gmail.com>
2023-02-13 18:10:16 +0300

    внес исправления ДЗ MyFirstGame

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -3,26 +3,21 @@ public class MyFirstGame {
         int numComp = 57;
         int numPlayer = 40;
 
-        if(numComp != numPlayer) {
-            if(numComp >= 0 && numComp < 100) {
-                while(numComp != numPlayer) {
-                    if(numPlayer > 99) {
-                        numPlayer = 0;
-                    } else if(numComp > numPlayer) {
-                        System.out.println("число: " + numPlayer + 
-                                " меньше того, что загадал компьютер");
-                    } else if(numComp < numPlayer) {
-                        System.out.println("число: " + numPlayer + 
-                                " больше того, что загадал компьютер");
-                    }
+        if(numComp > 0 && numComp <= 100) {
+            while(numComp != numPlayer) {
+                if(numComp > numPlayer) {
+                    System.out.println("число: " + numPlayer + 
+                            " меньше того, что загадал компьютер");
                     numPlayer++;
+                } else if(numComp < numPlayer) {
+                    System.out.println("число: " + numPlayer + 
+                            " больше того, что загадал компьютер");
+                    numPlayer--;
                 }
-                System.out.println("Вы победили!");
-            } else {
-                System.out.println("Вышли за полуинтервал");
             }
-        } else {
             System.out.println("Вы победили!");
+        } else {
+            System.out.println("Вышли за полуинтервал");
         }
     }
 }
