4188dd43bcc6aa59624c7ace82e50a815e4a9e1a
KororteevStanislav <koroteevsd@gmail.com>
2023-02-19 13:54:25 +0300

    внес исправления в ДЗ CyclesTheme

--- Lesson_1/CyclesTheme.java
+++ Lesson_1/CyclesTheme.java
@@ -5,16 +5,16 @@ public class CyclesTheme {
 
         int sumEven = 0;
         int sumOdd = 0;
-        int resultTask1 = -11;
+        int counter = -10;
 
         do {
-            resultTask1++;
-            if (resultTask1 % 2 == 0) {
-                sumEven += resultTask1;
+            if (counter % 2 == 0) {
+                sumEven += counter;
             } else {
-                sumOdd += resultTask1;
+                sumOdd += counter;
             }
-        } while (resultTask1 <= 20);
+            counter++;
+        } while (counter <= 21);
 
         System.out.println("В отрезке: от -10 до 21 сумма четных чисел = " + sumEven + "," 
                 + " а нечетных = " + sumOdd);
@@ -22,63 +22,53 @@ public class CyclesTheme {
         //Задача_2: Вывод чисел в интервале (min и max) в порядке убывания
         System.out.println("\n2.Вывод чисел в интервале (min и max) в порядке убывания\n");
 
-        int num1Task2 = 10;
-        int num2Task2 = 5;
-        int num3Task2 = -1;
-        int maxNumTask2;
-        int minNumTask2;
+        int num1 = 10;
+        int num2 = 5;
+        int num3 = -1;
+        int max = 0;
+        int min = 0;
 
-        if(num1Task2 > num2Task2 && num1Task2 > num3Task2) {
-            maxNumTask2 = num1Task2;
-        } else if(num2Task2 > num1Task2 && num2Task2 > num3Task2) {
-            maxNumTask2 = num2Task2;
+        if(num1 > num2) {
+            max = num1 > num3 ? num1 : num3;
+            min = num2 < num3 ? num2 : num3;
         } else {
-            maxNumTask2 = num3Task2;
+            max = num1 < num3 ? num1 : num3;
+            min = num2 > num3 ? num2 : num3;
         }
 
-        if(num1Task2 < num2Task2 && num1Task2 < num3Task2) {
-            minNumTask2 = num1Task2;
-        } else if(num2Task2 < num1Task2 && num2Task2 < num3Task2) {
-            minNumTask2 = num2Task2;
-        } else {
-            minNumTask2 = num3Task2;
-        }
-
-        for(int i = maxNumTask2 - 1; i > minNumTask2; i--) {
+        for(int i = max - 1; i > min; i--) {
             System.out.print(i + ", ");
         }
 
         //Задача_3: Вывод реверсивного числа и суммы его цифр
         System.out.println("\n\n3.Вывод реверсивного числа и суммы его цифр\n");
 
-        int num1Task3 = 1_234;
-        int reverseNum1Task3;
-        int sumOnesReverseNum1Task3 = 0;
+        int num4 = 1_234;
+        int sumRemainder = 0;
 
         System.out.print("Реверсивное число: ");
 
-        while(num1Task3 > 0) {
-            reverseNum1Task3 = num1Task3 % 10;
-            sumOnesReverseNum1Task3 += reverseNum1Task3;
-            num1Task3 /= 10;
-            System.out.print(reverseNum1Task3);
+        while(num4 > 0) {
+            int remainder = num4 % 10;
+            sumRemainder += remainder;
+            num4 /= 10;
+            System.out.print(remainder);
         }
 
-        System.out.println("\nСумма чисел равна: " + sumOnesReverseNum1Task3);
+        System.out.println("\nСумма чисел равна: " + sumRemainder);
 
         //Задача_4: Вывод чисел на консоль в несколько строк
         System.out.println("\n4.Вывод чисел на консоль в несколько строк\n");
 
-        int num1Task4 = 1;
-        int num2Task4 = 24;
+        int limitHalfInterval = 24;
 
-        for(int i = num1Task4; i < num2Task4;) {
+        for(int i = 1; i < limitHalfInterval;) {
             for(int y = 0; y < 5; y++) {
-                if(i < num2Task4) {
+                if(i < limitHalfInterval) {
                     System.out.printf("% 3d", i);
-                    i = i+2;
+                    i += 2;
                     if(y == 4) {
-                        System.out.print("\n");
+                        System.out.println();
                     }
                 } else {
                     System.out.printf("% 3d", 0);
@@ -89,23 +79,23 @@ public class CyclesTheme {
         //Задача_5: Проверка количества двоек на четность/нечетность
         System.out.println("\n\n5.Проверка количества двоек на четность/нечетность\n");
 
-        int num1Task5 = 3_242_592;
-        int numTwosInNum1Task5 = 0;
-        int sumTwosInNum1Task5 = 0;
+        int num5 = 3_242_592;
+        int numTwosInNum5 = 0;
+        int sumTwosInNum5 = 0;
 
-        System.out.print("Число: " + num1Task5 + " содержит ");
+        System.out.print("Число: " + num5 + " содержит ");
 
-        while(num1Task5 > 0) {
-            numTwosInNum1Task5 = num1Task5 % 10;
-            if(numTwosInNum1Task5 == 2) {
-                sumTwosInNum1Task5++;
+        while(num5 > 0) {
+            numTwosInNum5 = num5 % 10;
+            if(numTwosInNum5 == 2) {
+                sumTwosInNum5++;
             }
-            num1Task5 /= 10;
+            num5 /= 10;
         }
 
-        System.out.print("Двоек: " + sumTwosInNum1Task5);
+        System.out.print("Двоек: " + sumTwosInNum5);
 
-        if(sumTwosInNum1Task5 % 2 == 0) {
+        if(sumTwosInNum5 % 2 == 0) {
             System.out.print(" - четное количество");
         } else {
             System.out.print(" - нечетное количество");
@@ -118,37 +108,37 @@ public class CyclesTheme {
             for(int y = 0; y < 10; y++) {
                 System.out.print("*");
             }
-            System.out.print("\n");
+            System.out.println();
         }
 
-        System.out.print("\n");
+        System.out.println();
 
-        int sumHash = 5;
-        int sumHash2 = 5;
+        int numLines = 5;
+        int numCharInLines = 5;
 
-        while (sumHash > 0) {
-            sumHash--;
-            while(sumHash2 > 0) {
+        while (numLines > 0) {
+            numLines--;
+            while(numCharInLines > 0) {
                 System.out.print("#");
-                sumHash2--;
+                numCharInLines--;
             }
-            System.out.print("\n");
-            sumHash2 = sumHash;
+            System.out.println();
+            numCharInLines = numLines;
         }
 
-        System.out.print("\n");
+        System.out.println();
 
-        int numDollarSign = 0;
+        int numDollarSignInLines = 0;
 
         do {
             System.out.println("$");
-            numDollarSign++;
-            if(numDollarSign == 1 || numDollarSign == 3) {
+            numDollarSignInLines++;
+            if(numDollarSignInLines == 1 || numDollarSignInLines == 3) {
                 System.out.print("$");
-            } else if(numDollarSign == 2) {
+            } else if(numDollarSignInLines == 2) {
                 System.out.print("$$");
             }
-        } while(numDollarSign < 5);
+        } while(numDollarSignInLines < 5);
 
         //Задача_7: Отображение ASCII-символов
         System.out.println("\n7.Отображение ASCII-символов\n");
@@ -172,44 +162,38 @@ public class CyclesTheme {
         //Задача_8: Проверка, является ли число палиндромом
         System.out.println("\n8.Проверка, является ли число палиндромом\n");
 
-        int num1Task8 = 1_234_321;
-        int num2Task8 = num1Task8;
-        int reverseNum1Task8 = 0;
+        int num6 = 1_234_321;
+        int numPalindromeCheck = num6;
+        int reverse = 0;
 
-        System.out.print("Число: " + num1Task8 + " является ");
+        System.out.print("Число: " + num6 + " является ");
 
-        while(num2Task8 != 0) {
-            reverseNum1Task8 = reverseNum1Task8 * 10 + num2Task8 % 10;
-            num2Task8 /= 10;
+        while(numPalindromeCheck != 0) {
+            reverse = reverse * 10 + numPalindromeCheck % 10;
+            numPalindromeCheck /= 10;
         }
-        if(reverseNum1Task8 == num1Task8) {
-            System.out.print("палиндромом");
-        } else {
-            System.out.print("не палиндромом");
-        }
+
+        System.out.printf("%s", num6 == reverse ? "палиндромом" : "не палиндромом");
 
         //Задача_9: Определение, является ли число счастливым
         System.out.println("\n\n9.Определение, является ли число счастливым\n");
 
-        int num1Task9 = 467_359;
-        int sumFirstHalsNum1Task9 = 0;
-        int sumSecondHalsNum1Task9 = 0;
+        int num7 = 467_359;
+        int sumFirstHalf = 0;
+        int sumSecondHalf = 0;
 
-        System.out.print("Число: " + num1Task9);
+        System.out.print("Число: " + num7 + " является ");
         
         for(int i = 0; i < 3; i++) {
-            sumFirstHalsNum1Task9 = sumFirstHalsNum1Task9 + num1Task9 % 10;
-            num1Task9 /= 10;
+            sumFirstHalf += num7 % 10;
+            num7 /= 10;
         }
         for(int i = 0; i < 3; i++) {
-            sumSecondHalsNum1Task9 = sumSecondHalsNum1Task9 + num1Task9 % 10;
-            num1Task9 /= 10;
+            sumSecondHalf += num7 % 10;
+            num7 /= 10;
         }
-        if(sumFirstHalsNum1Task9 == sumSecondHalsNum1Task9) {
-            System.out.print(" является счастливым");
-        } else {
-            System.out.print(" является не счастливым");
-        }
+
+        System.out.printf("%s", sumFirstHalf == sumSecondHalf ? "счастливым" : "не счастливым");
 
         //Задача_10: Вывод таблицы умножения Пифагора
         System.out.println("\n\n10.Вывод таблицы умножения Пифагора\n");
@@ -236,7 +220,7 @@ public class CyclesTheme {
                     System.out.printf("% 3d", sum1Task10);
                 }
             }
-            System.out.print("\n");
+            System.out.println();
         }
     }
 }
